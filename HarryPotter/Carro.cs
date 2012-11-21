2f01505a78af39456afe691e3d3d1d2cc428e482
Francsico  calles <francalles@gmail.com>
2012-11-21 13:21:03 +0100

    Kata Potter

---
+++ HarryPotter/Carro.cs
@@ -0,0 +1,102 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace HarryPotter
+{
+    class Carro
+    {
+        const double  DECUENTO_2_LIBROS = 0.05;
+        const double DECUENTO_3_LIBROS = 0.10;
+        const double DECUENTO_4_LIBROS = 0.15;
+        const double DECUENTO_5_LIBROS = 0.20;
+        const double DECUENTO_6_LIBROS = 0.30;
+        const double DECUENTO_7_LIBROS = 0.45;
+        const int PRECIO_LIBRO = 8;
+
+        internal static double PrecioTotal(List<int> numLibros)
+        {
+            double precioTotal = 0; ;
+           
+            while (!EsVacioCarro(numLibros))
+            {
+                precioTotal = CalcularPrecioTotal(numLibros, precioTotal);        
+            }
+            
+            return precioTotal;
+        }
+
+        private static double CalcularPrecioTotal(List<int> numLibros, double precioTotal)
+        {
+            for (int i = 0; i < numLibros.Count; i++)
+            {
+                int numLibrosDiferentes = 1;
+
+                if (numLibros[i] > 0)
+                {
+                    precioTotal += CalcularDescuento(numLibros, i, ref numLibrosDiferentes);
+                    break;
+                }
+
+                if (numLibrosDiferentes != 1)
+                {
+                    break;
+                }
+            }
+            return precioTotal;
+        }
+
+        private static double CalcularDescuento(List<int> numLibros, int i, ref int numLibrosDiferentes)
+        {
+            for (int j = i + 1; j < numLibros.Count; j++)
+            {
+                if (numLibros[j] > 0)
+                {
+                    numLibrosDiferentes++;
+                    numLibros[j]--;
+                }
+            }
+            numLibros[i]--;
+            return CalcularPrecio(numLibrosDiferentes);
+        }
+
+        private static double CalcularPrecio(int numLibrosDiferentes)
+        {
+            return PRECIO_LIBRO * numLibrosDiferentes * (1 - DescuentoAplicado(numLibrosDiferentes));
+        }
+
+        private static bool EsVacioCarro(List<int> numLibros)
+        {
+            foreach (var item in numLibros)
+            {
+                if (item > 0)
+                {
+                    return false;
+                }
+            }
+            return true;
+        }
+
+        internal static double DescuentoAplicado(int numLibros) 
+        {
+            switch (numLibros)
+            {
+                case 2:
+                    return DECUENTO_2_LIBROS;
+                case 3:
+                    return DECUENTO_3_LIBROS;
+                case 4:
+                    return DECUENTO_4_LIBROS;
+                case 5:
+                    return DECUENTO_5_LIBROS;
+                case 6:
+                    return DECUENTO_6_LIBROS;
+                case 7:
+                    return DECUENTO_7_LIBROS;
+                default:
+                    return 0;
+            }
+        }
+    }
+}
---
+++ TestHarryPotter/ProgramTestProgram.cs
@@ -0,0 +1,212 @@
+﻿using HarryPotter;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+
+namespace TestHarryPotter
+{
+    
+    
+    /// <summary>
+    ///Se trata de una clase de prueba para ProgramTestProgram y se pretende que
+    ///contenga todas las pruebas unitarias ProgramTestProgram.
+    ///</summary>
+    [TestClass()]
+    public class ProgramTestProgram
+    {
+
+
+        private TestContext testContextInstance;
+
+        /// <summary>
+        ///Obtiene o establece el contexto de la prueba que proporciona
+        ///la información y funcionalidad para la ejecución de pruebas actual.
+        ///</summary>
+        public TestContext TestContext
+        {
+            get
+            {
+                return testContextInstance;
+            }
+            set            {
+                testContextInstance = value;
+            }
+        }
+
+        #region Atributos de prueba adicionales
+        // 
+        //Puede utilizar los siguientes atributos adicionales mientras escribe sus pruebas:
+        //
+        //Use ClassInitialize para ejecutar código antes de ejecutar la primera prueba en la clase 
+        //[ClassInitialize()]
+        //public static void MyClassInitialize(TestContext testContext)
+        //{
+        //}
+        //
+        //Use ClassCleanup para ejecutar código después de haber ejecutado todas las pruebas en una clase
+        //[ClassCleanup()]
+        //public static void MyClassCleanup()
+        //{
+        //}
+        //
+        //Use TestInitialize para ejecutar código antes de ejecutar cada prueba
+        //[TestInitialize()]
+        //public void MyTestInitialize()
+        //{
+        //}
+        //
+        //Use TestCleanup para ejecutar código después de que se hayan ejecutado todas las pruebas
+        //[TestCleanup()]
+        //public void MyTestCleanup()
+        //{
+        //}
+        //
+        #endregion
+
+
+        List<int> numLibros = new List<int> { 0, 0, 0, 0, 0, 0, 0 };
+        double total = 0;
+
+        /// <summary>
+        ///Prueba carro vacio
+        ///</summary>
+        [TestMethod()]
+        [DeploymentItem("HarryPotter.exe")]
+        public void Carro_vacio()
+        {
+            //Preparacion
+
+            //Actuacion
+            double result = HarryPotter.Carro.PrecioTotal(numLibros);
+            //Confirmacion
+            Assert.AreEqual(total, result);
+        }
+
+
+        /// <summary>
+        /// Compra de un libro
+        ///</summary>
+        [TestMethod()]
+        [DeploymentItem("HarryPotter.exe")]
+        public void Compra_Un_Libro()
+        {
+            //Preparacion
+            total = 8;
+            numLibros[0] = 1;
+            //Actuacion
+            double result = HarryPotter.Carro.PrecioTotal(numLibros);
+            //Confirmacion
+            Assert.AreEqual(total, result);
+        }
+
+
+        /// <summary>
+        /// Compra de un libro
+        ///</summary>
+        [TestMethod()]
+        [DeploymentItem("HarryPotter.exe")]
+        public void Compra_N_Libros_Iguales()
+        {
+            //Preparacion
+            total = 16;
+            numLibros[0] = 2;
+            //Actuacion
+            double result = HarryPotter.Carro.PrecioTotal(numLibros);
+            //Confirmacion
+            Assert.AreEqual(total, result);
+        }
+
+
+        /// <summary>
+        /// Compra de un libro
+        ///</summary>
+        [TestMethod()]
+        [DeploymentItem("HarryPotter.exe")]
+        public void Decuento_2_Libros_Diferentes()
+        {
+            //Preparacion
+            double Descuento = 0.05;
+            int numLibrosDiferentes = 2;
+            //Actuacion
+            double result = HarryPotter.Carro.DescuentoAplicado(numLibrosDiferentes);
+            //Confirmacion
+            Assert.AreEqual(Descuento, result);
+        }
+
+        /// <summary>
+        /// Compra de un libro
+        ///</summary>
+        [TestMethod()]
+        [DeploymentItem("HarryPotter.exe")]
+        public void Decuento_3_Libros_Diferentes()
+        {
+            //Preparacion
+            double Descuento = 0.10;
+            int numLibrosDiferentes = 3;
+            //Actuacion
+            double result = HarryPotter.Carro.DescuentoAplicado(numLibrosDiferentes);
+            //Confirmacion
+            Assert.AreEqual(Descuento, result);
+        }
+
+        /// <summary>
+        /// Compra de un libro
+        ///</summary>
+        [TestMethod()]
+        [DeploymentItem("HarryPotter.exe")]
+        public void Compra_2_Diferentes()
+        {
+            //Preparacion
+            numLibros[0] = 1;
+            numLibros[1] = 1;
+            total = 8 * 2 * 0.95;
+            //Actuacion
+            double result = HarryPotter.Carro.PrecioTotal(numLibros);
+            //Confirmacion
+            Assert.AreEqual(total, result);
+        }
+
+        /// <summary>
+        /// Compra de un libro
+        ///</summary>
+        [TestMethod()]
+        [DeploymentItem("HarryPotter.exe")]
+        public void Compra_3_Diferentes()
+        {
+            //Preparacion
+            numLibros[0] = 1;
+            numLibros[1] = 1;
+            numLibros[2] = 1;
+            total = 8 * 3 * 0.90;
+            //Actuacion
+            double result = HarryPotter.Carro.PrecioTotal(numLibros);
+            //Confirmacion
+            Assert.AreEqual(total, result);
+        }
+
+
+        /// <summary>
+        /// Compra de un libro
+        ///</summary>
+        [TestMethod()]
+        [DeploymentItem("HarryPotter.exe")]
+        public void Compra_N_Diferentes()
+        {
+            //Preparacion
+            numLibros[0] = 1;
+            numLibros[1] = 2;
+            numLibros[2] = 3;
+            numLibros[3] = 4;
+            numLibros[4] = 5;
+            numLibros[5] = 6;
+            numLibros[6] = 7;
+
+            total = 168.4;
+            //Actuacion
+            double result = HarryPotter.Carro.PrecioTotal(numLibros);
+            //Confirmacion
+            Assert.AreEqual(total, result);
+        }
+
+    }
+}
